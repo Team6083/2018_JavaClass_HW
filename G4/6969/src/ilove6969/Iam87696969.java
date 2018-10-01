c47f2cd9153c8b1b38a168e335f6ec56dc57fe58
Eric <Eric@LAPTOP-L3J7MHC8>
2018-10-01 22:28:08 +0800

    Revert "G4"
    This reverts commit 2210e7321b8d6893d849bbffbe7fa933cb7f2fae.

--- G4/6969/src/ilove6969/Iam87696969.java
+++ G4/6969/src/ilove6969/Iam87696969.java
@@ -4,12 +4,7 @@ public class Iam87696969 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 15;
-		int b = 3;
 
-		System.out.println(a + b);
-		System.out.println(a - b);
-		System.out.println(a * b);
-		System.out.println(a / b);
 	}
+
 }
