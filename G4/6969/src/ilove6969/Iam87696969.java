ae4ab489d55a9a187d079a4226a4904e1d07b587
Eric <Eric@LAPTOP-L3J7MHC8>
2018-10-01 22:34:45 +0800

    G4

--- G4/6969/src/ilove6969/Iam87696969.java
+++ G4/6969/src/ilove6969/Iam87696969.java
@@ -4,6 +4,12 @@ public class Iam87696969 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+int a=0;
+int b=5;
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a*b);
+System.out.println(a/b);
 
 	}
 
